e93bcba2cba47e98e2a4a4d649a33d96b2ed724c
MustafaSultanov <sultanovmusttafa@gmail.com>
2025-10-26 18:43:57 +0600

    fixed

--- components/pages/Porjects/index.tsx
+++ components/pages/Porjects/index.tsx
@@ -22,7 +22,7 @@ const projects: Project[] = [
 	{
 		title: "Akylman",
 		description:
-			"LMS is a learning management system for managing online and offline training c",
+			"LMS is a learning management system for managing online and offline ",
 		techStack: [
 			"TypeScript",
 			"Redux",
